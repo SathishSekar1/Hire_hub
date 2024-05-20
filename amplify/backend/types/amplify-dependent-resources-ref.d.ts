d874d391b9a3659829663b058fc9c3bc783bcd05
Sathish <sekar.sathish2012@gmail.com>
2024-05-20 17:07:52 +0100

    updated to with cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mltradingwebsite90e0d6e3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// From here Added by Sathish
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports'; // Your Amplify configuration file
+import '@aws-amplify/ui-react/styles.css'; // Import Amplify CSS for the authenticator
+
+Amplify.configure(config); // Configure Amplify with your Cognito settings
+
+// Up to here Added by Sathish
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
